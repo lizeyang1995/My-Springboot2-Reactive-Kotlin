eae3eba0e4f9eee22e5a5afb9a59a22c252902c9
lizeyang1995 <424011527@qq.com>
2020-12-23 14:32:57 +0800

    first commit

---
+++ src/main/kotlin/com/example/Myspringboot2reactivekotlin/MySpringboot2ReactiveKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Myspringboot2reactivekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MySpringboot2ReactiveKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MySpringboot2ReactiveKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/Myspringboot2reactivekotlin/MySpringboot2ReactiveKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Myspringboot2reactivekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySpringboot2ReactiveKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
